cf879c094d93c2453cad5b974c3cfd3caa68171d
s23sean321 <141469984+s23sean321@users.noreply.github.com>
2023-08-05 15:29:27 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,34 +1,39 @@
-from flask import Flask,request,abort
-from line_bot_api import *
+from flask import Flask, request, abort
+
 from events.basic import *
+from line_bot_api import *
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-@app.route("/callback",methods=['POST'])
+@app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.feaders['X-Line-Signature']
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
+    # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: "+body)
+    app.logger.info("Request body: " + body)
 
+    # handle webhook body
     try:
-        handler.handle(body,signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
-        print("Invalid signature. Plesae cheak your channel access token/channel secret.")
+        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-    
-    return 'ok'
 
-@handler.add(MessageEvent,message=TextMessage)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
     message_text = str(event.message.text).lower()
 
-    if message_text=='@關於我們':
+    if message_text == '@關於我們':
         about_us_event(event)
 
-    elif message_text=='@營業據點':
+    elif message_text == '@營業據點':
         location_event(event)
 
-
+        
 if __name__ == "__main__":
-    app.run
+    app.run()
--- events/basic.py
+++ events/basic.py
@@ -1,5 +1,6 @@
 from line_bot_api import *
 
+
 def about_us_event(event):
     emoji = [
         {
@@ -13,35 +14,40 @@ def about_us_event(event):
             "emojiId": "225"
         }
     ]
-    text_message = TextSendMessage(text='''$ 廣島冰茶鋪 $
-    讓你不小心斷片~~~~''',emojis=emoji)
+
+    text_message = TextSendMessage(text='''$ Master SPA $
+專業中醫推拿出身，融合東西方按摩手法
+
+-嚴格把關：所有用品皆有消毒或採一次用品。
+
+-設備齊全：夏天有冷氣，冬天有電毯和暖氣。
+
+-獨立空間：專業乾淨高品質獨立按摩空間。''', emojis=emoji)
 
     sticker_message = StickerSendMessage(
-        package_id='5ac21c46040ab15980c9b442',
-        sticker_id='003'
+        package_id='8522',
+        sticker_id='16581271'
     )
 
     about_us_img = 'https://i.imgur.com/70A4WdI.jpg'
 
     image_message = ImageSendMessage(
         original_content_url=about_us_img,
-        preview_inage_url=about_us_img
+        preview_image_url=about_us_img
     )
 
     line_bot_api.reply_message(
         event.reply_token,
-        [text_message,sticker_message,image_message]
-    )
-
+        [text_message, sticker_message, image_message])
+    
 def location_event(event):
     location_message = LocationSendMessage(
-        title='廣島冰茶鋪',
-        address='80049高雄市新興區中山一路243號',
-        latitude=22.635115780380566, 
-        longitude=120.3019637852388
+        title='Master SPA',
+        address='110台北市信義區信義路五段7號',
+        latitude=25.0333695,
+        longitude=121.5638839
     )
 
     line_bot_api.reply_message(
         event.reply_token,
-        location_message
-    )
+        location_message)
