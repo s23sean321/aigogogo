2f2794f33d4518e3fc74fdd81a3798a93b085015
s23sean321 <141469984+s23sean321@users.noreply.github.com>
2023-08-22 05:08:16 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, abort
+
+from events.basic import *
+from line_bot_api import *
+
+app = Flask(__name__)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+
+    message_text = str(event.message.text).lower()
+
+    if message_text == '@關於我們':
+        about_us_event(event)
+
+    elif message_text == '@營業據點':
+        location_event(event)
+
+        
+if __name__ == "__main__":
+    app.run()
