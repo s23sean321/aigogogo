1a0ef4e7201c3ed7f99f0ab208a791cd5a0587e0
s23sean321 <141469984+s23sean321@users.noreply.github.com>
2023-08-22 13:57:38 +0800

    Add files via upload

--- events/basic.py
+++ events/basic.py
@@ -9,7 +9,7 @@ def about_us_event(event):
             "emojiId": "225"
         },
         {
-            "index": 13,
+            "index": 10,
             "productId": "5ac21184040ab15980c9b43a",
             "emojiId": "225"
         }
