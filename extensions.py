71df15b3576a410a4fc94c77dfe1602b80ecb00a
s23sean321 <141469984+s23sean321@users.noreply.github.com>
2023-08-26 10:33:06 +0800

    Add files via upload

--- extensions.py
+++ extensions.py
@@ -1,5 +1,9 @@
+#pip install flask_sqlalchemy
+#pip install psycopg2-binary 連線的套件
+#pip install Flask-Migrate 管理資料庫的套件
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 db = SQLAlchemy()
-migrate = Migrate()
+migrate = Migrate() 
